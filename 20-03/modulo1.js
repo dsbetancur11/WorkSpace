9f6888619d9e9b6ec2c7758ac6f8aee6443c57ce
dsbetancur11 <darwinbetancurv@ielacandelariamedellin.edu.co>
2026-03-20 21:03:15 +0000

    Ejercicios js

---
+++ 20-03/modulo1.js
@@ -0,0 +1,13 @@
+var nombre = "Darwin";
+let edad = 16;
+const ciudad = "medellin";
+console.log("nombre:");
+console.log(nombre);
+console.log("edad:");
+console.log(edad);
+console.log("ciudad:");
+console.log(ciudad);
+console.log("la edad es:");
+console.log(edad);
+console.log("ciudad: ", ciudad);
+console.log("nombre: ", nombre, "edad: ", edad, "ciudad: ", ciudad);
